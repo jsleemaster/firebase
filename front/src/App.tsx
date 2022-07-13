36020cc4f601b4b6b9f6b790bb9e303e8ae2a74b
이순명 <roeowo123@naver.com>
2022-07-13 14:32:29 +0900

    react-redux

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,37 +1,17 @@
 import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const test = (e:Event) =>{
+    console.log(e)
+  }
+  const test2 = (e:Event) => {
+    console.log(e)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello Vite + React!</p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="App" >
+      <div onDragStart={test} onDragEnd={test2} className="test" draggable="true"></div>
     </div>
   )
 }

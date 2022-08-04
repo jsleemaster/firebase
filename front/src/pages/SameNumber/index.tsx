5b6d4d9f499715c447c879315f66a63f1297210c
jsleemaster <roeowo123@naver.com>
2022-08-04 15:18:16 +0900

    하드코딩 값 변경

--- front/src/pages/SameNumber/index.tsx
+++ front/src/pages/SameNumber/index.tsx
@@ -1,18 +1,22 @@
 import { useState } from 'react'
+
 const shuffle = (arr : number[]) => {
   if (arr.length === 0 ) return arr;
   return arr.sort(() => Math.random() - 0.5);
 }
+
 function Index() {
-  const randomBox1 = [1,2,3,4,5,6,7,8,9]
-  const randomBox2 = [1,2,3,4,5,6,7,8,9]
+  const randomBox1 = Array(9).fill(0).map((v,i)=>i+1)
+  const randomBox2 = Array(9).fill(0).map((v,i)=>i+1)
 
   const concatBox = randomBox1.concat(randomBox2)
   const shuffleBox = shuffle(concatBox)
-  console.log(shuffleBox)
 
   return (
-    <div className='flex flex-col justify-center items-center'>
+    <div className='flex flex-col justify-center items-center' >
+      {shuffleBox.map((v,i)=>{
+        return <div key={i}>{v}</div>
+      })}
     </div>
   )
 }

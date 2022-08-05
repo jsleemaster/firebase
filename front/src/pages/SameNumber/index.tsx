20a323805189907fe8f2e504e868a5e6181092a9
jsleemaster <roeowo123@naver.com>
2022-08-05 17:37:59 +0900

    디자인 수정

--- front/src/pages/SameNumber/index.tsx
+++ front/src/pages/SameNumber/index.tsx
@@ -4,20 +4,23 @@ const shuffle = (arr : number[]) => {
   if (arr.length === 0 ) return arr;
   return arr.sort(() => Math.random() - 0.5);
 }
-
 function Index() {
   const randomBox1 = Array(9).fill(0).map((v,i)=>i+1)
   const randomBox2 = Array(9).fill(0).map((v,i)=>i+1)
 
   const concatBox = randomBox1.concat(randomBox2)
   const shuffleBox = shuffle(concatBox)
-
+  
   return (
-    <div className='flex flex-col justify-center items-center' >
+    <>
+    <div className='flex flex-wrap justify-center items-center w-full' >
       {shuffleBox.map((v,i)=>{
-        return <div key={i}>{v}</div>
+        return <div 
+        className='w-32 h-32 flex items-center justify-center' key={i} 
+        >{v}</div>
       })}
     </div>
+    </>
   )
 }
 

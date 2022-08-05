a326e805128c708999cd277de6354453f172e858
jsleemaster <roeowo123@naver.com>
2022-08-06 08:29:02 +0900

    type 지정

--- front/src/pages/SameNumber/index.tsx
+++ front/src/pages/SameNumber/index.tsx
@@ -4,21 +4,25 @@ const shuffle = (arr : number[]) => {
   if (arr.length === 0 ) return arr;
   return arr.sort(() => Math.random() - 0.5);
 }
+interface Shuffle {
+  name? : number,
+
+}
 function Index() {
   const randomBox1 = Array(9).fill(0).map((v,i)=>i+1)
   const randomBox2 = Array(9).fill(0).map((v,i)=>i+1)
 
   const concatBox = randomBox1.concat(randomBox2)
-  const shuffleBox = shuffle(concatBox)
-  
+  const shuffleArr = shuffle(concatBox)
+  let shuffleBox:Shuffle = {}
   return (
     <>
     <div className='flex flex-wrap justify-center items-center w-full' >
-      {shuffleBox.map((v,i)=>{
+      {/* {shuffleBox.map((v,i)=>{
         return <div 
         className='w-32 h-32 flex items-center justify-center' key={i} 
         >{v}</div>
-      })}
+      })} */}
     </div>
     </>
   )

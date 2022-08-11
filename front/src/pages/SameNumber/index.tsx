e3c305f8fcdc72fe8cef43c76beb22a0c2936262
jsleemaster <roeowo123@naver.com>
2022-08-11 17:23:32 +0900

    랜덤 컬러값

--- front/src/pages/SameNumber/index.tsx
+++ front/src/pages/SameNumber/index.tsx
@@ -4,9 +4,9 @@ const shuffle = (arr : number[]) => {
   if (arr.length === 0 ) return arr;
   return arr.sort(() => Math.random() - 0.5);
 }
-interface Shuffle {
-  name? : number,
-
+interface shuffle {
+  number? : number,
+  style? : object
 }
 function Index() {
   const randomBox1 = Array(9).fill(0).map((v,i)=>i+1)
@@ -14,15 +14,28 @@ function Index() {
 
   const concatBox = randomBox1.concat(randomBox2)
   const shuffleArr = shuffle(concatBox)
-  let shuffleBox:Shuffle = {}
+  const newArr:object[] = []
+  
+  const color = ['red','gray','green','blue']
+  shuffleArr.map((v)=>{
+    let obj:shuffle = {}
+    obj.number = v
+    obj.style = {
+      color : color[Math.floor(Math.random() * color.length-1)],
+      backgroundColor : '#F9F9F9'
+    }
+    newArr.push(obj)
+  })
+
   return (
     <>
     <div className='flex flex-wrap justify-center items-center w-full' >
-      {/* {shuffleBox.map((v,i)=>{
+      {newArr.map((v,i)=>{
         return <div 
-        className='w-32 h-32 flex items-center justify-center' key={i} 
-        >{v}</div>
-      })} */}
+        className='w-32 h-32 flex items-center justify-center' key={i}
+        style={v.style} 
+        >{v.number}</div>
+      })}
     </div>
     </>
   )

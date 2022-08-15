a98d7386115c32251df9123f0df433774b8e9aea
jsleemaster <roeowo123@naver.com>
2022-08-15 16:41:16 +0900

    useEffect 활용한 박스 상태관리 변경

--- front/src/pages/SameNumber/index.tsx
+++ front/src/pages/SameNumber/index.tsx
@@ -1,39 +1,45 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 
-const shuffle = (arr : number[]) => {
-  if (arr.length === 0 ) return arr;
-  return arr.sort(() => Math.random() - 0.5);
-}
-interface shuffle {
+const shuffle = (arr: Array<number>) => {
+  if (arr.length === 0) return [];
+  return arr.slice().sort(() => Math.random() - 0.5);
+};
+
+
+interface Shuffle {
   number? : number,
   style? : object
 }
-function Index() {
-  const randomBox1 = Array(9).fill(0).map((v,i)=>i+1)
-  const randomBox2 = Array(9).fill(0).map((v,i)=>i+1)
 
-  const concatBox = randomBox1.concat(randomBox2)
-  const shuffleArr = shuffle(concatBox)
-  // let boxArr:object[] = []
-  const [boxArr, setBoxArr] = useState<object[]>([])
-  
-  const color = ['red','gray','green','blue']
-  shuffleArr.map((v)=>{
-    let obj:shuffle = {}
-    obj.number = v
-    obj.style = {
-      color : color[Math.floor(Math.random() * color.length-1)],
-      backgroundColor : '#F9F9F9',
-      cursor : 'pointer'
-    }
-    boxArr.push(obj)
-  })
-  let one:number[] = [];  
-  const clickbox = (number:number, index:number) => {
-    if ( one.length !== 0 ) {
+const color = ['red','gray','green','blue'];
+
+function Index() {
+  const [boxList, setBoxList] = useState<Shuffle[]>([]);
+
+  useEffect(() => {
+    const randomBox = Array(9).fill(0).map((_, i) => i + 1);
+
+    const shuffleList = shuffle([...randomBox, ...randomBox]).map((v)=>{
+      return {
+        number: v,
+        style: {
+          color : color[Math.floor(Math.random() * color.length -1 )],
+          backgroundColor : '#F9F9F9',
+          cursor : 'pointer'
+        }
+      };
+    });
+
+    setBoxList(shuffleList);
+  }, []);
+
+  let one: number[] = [];
+
+  const clickBox = (number: number, index: number) => {
+    if (one.length !== 0) {
       //두번째 클릭
-      if ( one.findIndex((v)=>v === number) !== -1) {
-        one = []
+      if (one.findIndex((v)=>v === number) !== -1) {
+        one = [];
         //삭제로직
       } else {
         return;
@@ -41,20 +47,23 @@ function Index() {
     } else {
       one.push(number)
     }
-  }
+  };
+
   return (
     <>
-    <div className='flex flex-wrap justify-center items-center w-full' >
-      {boxArr.map((v,i)=>{
-        return <div 
-        className='w-32 h-32 flex items-center justify-center' key={i}
-        style={v.style} 
-        onClick={()=>clickbox(v.number,i)}
-        >{v.number}</div>
-      })}
-    </div>
+      <div className='flex flex-wrap justify-center items-center w-full' >
+        {boxList.map((v,i) => (
+          <div
+            className='w-32 h-32 flex items-center justify-center'
+            key={i}
+            style={v.style} 
+            onClick={() => clickBox(v.number, i)}
+          >{v.number}</div>
+        ))}
+      </div>
     </>
-  )
+  );
 }
+
 
 export default Index
